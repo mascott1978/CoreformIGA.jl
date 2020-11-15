e15f01ec3432ed6fc95e968c3242b4c226a41553
Documenter.jl <documenter@juliadocs.github.io>
2020-11-15 04:41:54 +0000

    build based on 5e88259

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CoreformIGA","category":"page"},{"location":"#CoreformIGA","page":"Home","title":"CoreformIGA","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CoreformIGA BernsteinBasis]\nBernsteinBasis.basisValue(i, p, xi)","category":"page"},{"location":"#CoreformIGA.BernsteinBasis.basisValue-Tuple{Any,Any,Any}","page":"Home","title":"CoreformIGA.BernsteinBasis.basisValue","text":"basisValue(i, p, xi)\n\ndx = 5\n\n\n\n\n\n","category":"method"}]
+}
