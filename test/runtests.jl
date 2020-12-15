01acd8dfc2e19202b98ef92dee980bcc224b54e0
zhihui <zhihuizou1988@gmail.com>
2020-12-15 16:26:26 -0600

    add new tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -47,7 +47,29 @@ using Test
         @test bm_fc.local_basis_parametric_gradient( 1, 0.5 )[ 2 ] == 1.0
     end
     @testset "BasisSpline.jl" begin
-        #make a basis spline function collection
-        @test bs.x == y
+        layout = CoreformIGA.BasisMesh.layout_bspline_1d_uniform_h_max_k( 1, 2, domain = [ 0, 2 ] )
+        bm_fc = CoreformIGA.BasisMesh.function_collection( layout )
+        bs_fc = CoreformIGA.BasisSpline.function_collection( bm_fc )
+        @test bs_fc.global_basis_value( 1, 0.5 ) == [0.5, 0.5]
+        @test bs_fc.global_basis_value( 2, 0.4 ) == [0.6, 0.4]
+        @test bs_fc.global_basis_parametric_gradient( 1, 0.5 ) == [-1, 1]
+        @test bs_fc.global_basis_parametric_gradient( 2, 0.4 ) == [-1, 1]
+    end
+    @testset "Field.jl" begin
+        layout = CoreformIGA.BasisMesh.layout_bspline_1d_uniform_h_max_k( 1, 2, domain = [ 0, 2 ] )
+        bm_fc = CoreformIGA.BasisMesh.function_collection( layout )
+        bs_fc = CoreformIGA.BasisSpline.function_collection( bm_fc )
+        nodes = [ [0.0, 0.0, 0.0], [2.0, 0.0, 0.0], [4.0, 0.0, 0.0] ]
+        field_fc = CoreformIGA.Field.function_collection( bm_fc, bs_fc, nodes )
+        @test field_fc.node_on_element( 1, 1 ) == [0.0, 0.0, 0.0]
+        @test field_fc.node_on_element( 1, 2 ) == [2.0, 0.0, 0.0]
+        @test field_fc.node_on_element( 2, 1 ) == [2.0, 0.0, 0.0]
+        @test field_fc.node_on_element( 2, 2 ) == [4.0, 0.0, 0.0]
+        @test field_fc.nodes_on_element( 1 ) == [ [0.0, 0.0, 0.0], [2.0, 0.0, 0.0] ]
+        @test field_fc.nodes_on_element( 2 ) == [ [2.0, 0.0, 0.0], [4.0, 0.0, 0.0] ]
+        @test field_fc.field_value( 1, 0.5 ) == [ 1.0, 0.0, 0.0 ]
+        @test field_fc.field_value( 2, 0.5 ) == [ 3.0, 0.0, 0.0 ]
+        @test field_fc.field_parametric_gradient( 1, 0.5 ) == [ 2.0, 0.0, 0.0 ]
+        @test field_fc.field_parametric_gradient( 2, 0.5 ) == [ 2.0, 0.0, 0.0 ]
     end
 end
