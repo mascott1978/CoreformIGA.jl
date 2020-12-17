e0defdc71074d58e574073f6b6d7e8d1b27ba6ee
zhihui <zhihuizou1988@gmail.com>
2020-12-17 13:18:21 -0600

    one element example works now.

--- class/Examples.jl
+++ class/Examples.jl
@@ -3,14 +3,12 @@
 
 
 import CoreformIGA
-layout_interior = CoreformIGA.BasisMesh.bspline( 1, 1 )
+layout_interior = CoreformIGA.BasisMesh.layout_bspline_1d_uniform_h_max_k( 1, 1 )
 nodes_interior = [0,1]
 quad_rules_interior = [2]
 
-layout_constraint_bdry = CoreformIGA.BasisMesh.vertex()
 nodes_constraint_bdry = [0]
 
-layout_traction_bdry = CoreformIGA.BasisMesh.vertex()
 nodes_traction_bdry = [1]
 
 chi(x) = x >= 0 && x <= 1 ? 1.0 : 1e-12
@@ -21,8 +19,22 @@ A(x) = 1
 load(x) = 0
 traction(x) = 1
 
-K, M, B, F, G, H = CoreformIGA.FlexRepresentationMethod1d.assemble( layout_interior, nodes_interior, quad_rules_interior,
-                                                                    layout_constraint_bdry, nodes_constraint_bdry,
-                                                                    layout_traction_bdry, nodes_traction_bdry,
-                                                                    chi, penalty_constraint, constraint, E, A, load, traction)
+K, M, B, F, G, H = CoreformIGA.FlexRepresentationMethod1d.assemble( layout_interior,
+                                                                    nodes_interior,
+                                                                    quad_rules_interior,
+                                                                    nodes_constraint_bdry,
+                                                                    nodes_traction_bdry,
+                                                                    chi,
+                                                                    penalty_constraint,
+                                                                    constraint,
+                                                                    E,
+                                                                    A,
+                                                                    load,
+                                                                    traction )
 
+println( K )
+println( M )
+println( B )
+println( F )
+println( G )
+println( H )
