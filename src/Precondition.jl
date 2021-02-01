caeb17fb7d2cf307b8571e935ffb58122650982c
zhihui <zhihuizou1988@gmail.com>
2021-02-01 17:19:44 -0600

    bug fixes.

--- src/Precondition.jl
+++ src/Precondition.jl
@@ -7,12 +7,9 @@ function precondition( K, r )
     while ~isempty( depend_sets ) # found linearly independent basis
         grouped_depend_sets = group_dependency( depend_sets_temp )
         # orthonormalize
-        row_num, col_num = size( K_star )
-        G = Matrix(I, row_num, col_num )
         for depend_fns in grouped_depend_sets
-            G[depend_fns, depend_fns] = orthonormalize( K, depend_fns  )
+            S[depend_fns, depend_fns] = orthonormalize( K[ depend_fns, depend_fns ]  )
         end
-        S = G*D
         depend_sets = identify_dependency( S*K*(S'), r )
         depend_sets_temp = append!( depend_sets_temp, depend_sets )
     end
@@ -23,14 +20,14 @@ function scale( K )
     D = Diagonal( 1 ./ sqrt.( diag( K ) ) );
 end
 
-function identify_dependency( K_star, r )
+function identify_dependency( K, r )
     depend_sets = []
-    row_num, col_num = size( K_star )
+    row_num, col_num = size( K )
     for irow = 1:row_num
         i_depend_set = [ irow ]
         for icol = irow:col_num
-            if K_star( irow, icol ) > r
-                push!(i_depend_set, icol)
+            if K( irow, icol ) > r
+                push!(i_depend_set, icol )
             end
         end
         if length( i_depend_set ) > 1
@@ -43,36 +40,38 @@ function group_dependency( depend_sets )
     if length( depend_sets ) == 1
         return depend_sets
     else
-        i = 1
-        while i > 0
+        anchor = 2
+        set_l = length( depend_sets )
+        while anchor <= set_l
             intersected_set_id = []
-            for j = ( i + 1 ):length( depend_sets )
-                intersection = intersect( depend_sets[ i ], depend_sets[ j ] )
+            for j = anchor:set_l
+                intersection = intersect( depend_sets[ anchor-1 ], depend_sets[ j ] )
                 if ~isempty( intersection )
                     push!( intersected_set_id, j )
                 end
             end
-            deleteat!( depend_sets, intersected_set_id )
-            if length( depend_sets ) > ( i + 1 )
-               i = i + 1
+            if isempty( intersected_set_id )
+                anchor = anchor + 1
             else
-                i = -1 # search is done
+                push!( depend_sets[ anchor - 1 ], vcat( depend_sets[ intersected_set_id ] ) )
+                unique!( depend_sets[ anchor - 1 ] )
+                deleteat!( depend_sets, intersected_set_id )
+                set_l = length( depend_sets )
             end
         end
         return depend_sets
     end
 end
 
-function orthonormalize( K, depend_fns  )
-    row_num = length( depend_fns )
-    S_sigma = scale( K[ depend_fns, depend_fns ] )
-    G = Matrix( I, row_num, row_num )
+function orthonormalize( K  )
+    row_num = size( K, 1 )
+    S_sigma = scale( K )
+    K_star = S_sigma * K * ( S_sigma' )
     for irow = 1:row_num
         # orthogonalize
-        G[ irow, 1:( irow - 1 ) ] = -K_star[ depend_fns[ irow ], depend_fns[ 1:( irow - 1 ) ] ]
+        S_sigma[ irow, 1:( irow - 1 ) ] = S_sigma - K_star[ irow, 1: irow ]
     end
-    K_perb = G * K_star * ( G')
     # normalize
-    G = G./diag( K_perb )
-    return G
+    S_sigma = S_sigma./diag( S_sigma )
+    return S_sigma
 end
